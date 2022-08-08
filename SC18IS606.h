2be30c96772acf4562645b6066e8c582f7846659
Brendan <103772579+bmkuzior521@users.noreply.github.com>
2022-08-08 15:34:25 -0400

    Update SC18IS606.h
    added description

--- SC18IS606.h
+++ SC18IS606.h
@@ -8,12 +8,15 @@
 #ifndef SC18IS606_H_
 #define SC18IS606_H_
 
+//This .h file requires customization for their purpose according to the SC18IS606 data Sheet. 
+//(x.x.x) refer to section of data sheet
+
 /*
  * ======== SC18IS606 Registers ==========
  */
 
 #define bridge_config_fxn       0xF0;   //SC18 fxn id for SPI configuration
-#define bridge_config           0x06;   //Change this value for SPI config (Table 5) AD5941 = 1101 0x0d
+#define bridge_config           0x06;   //Change this value for SPI config (Table 5) 
 
 #define clear_int               0xF1;   //Byte clears interrupt (7.1.6)
 
@@ -31,10 +34,10 @@
 #define SC18_GPIO_CONFIG_FXN    0xF7;   //FXN id byte for GPIO configuration
 #define SC18_GPIO_CONFIG        0x00;   //Config bits as referenced in table 10 and 11
 
-#define SC18_ADDR               0x28;   //7-bit Write address for SC18 I2C Tx function will handle read and write values
-#define SC18_R_ADDR             0x51;
+#define SC18_ADDR               0x28;   //7-bit Write address for SC18 I2C Tx function will handle read and write values (0x50)
+#define SC18_R_ADDR             0x29;   //7-bit Read address
 
-#define SC18_WRITE_EN           0x06;   //Write Enable Function ID for EEPROM on bridge
+#define SC18_WRITE_EN           0x06;   //Write Enable Function ID for EEPROM on development board
 
 #define SC18_CS                 0x04;   //chip select, 01-07
 
